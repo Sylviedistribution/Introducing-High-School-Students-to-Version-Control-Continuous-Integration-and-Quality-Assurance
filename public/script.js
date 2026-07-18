91111cbb2dc04821f968517c918ff5824eff81da
Loutfia <loutfia.badarou@groupe-esigelec.org>
2026-07-18 09:04:49 +0000

    Implement number input

--- public/script.js
+++ public/script.js
@@ -14,3 +14,23 @@ function updateDisplay() {
 	expressionEl.textContent = formatDisplay(expression);
 	resultEl.textContent = formatDisplay(currentValue);
 }
+
+function isOperator(char) {
+	return ["+", "-", "*", "/"].includes(char);
+}
+
+function appendDigit(digit) {
+	if (justEvaluated) {
+		expression = "";
+		currentValue = "0";
+		justEvaluated = false;
+	}
+
+	if (currentValue === "0" && !expression.length) {
+		currentValue = digit;
+	} else if (currentValue === "0" && expression.length && !isOperator(expression.at(-1))) {
+		currentValue = digit;
+	} else {
+		currentValue = currentValue === "0" ? digit : `${currentValue}${digit}`;
+	}
+}
